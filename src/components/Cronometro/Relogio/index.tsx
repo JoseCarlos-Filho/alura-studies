c51fd1c1224f0b45c8556aed5410ad01ee0444fc
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-11-20 22:46:43 -0300

    Refatora componente Cronometro para gerenciar o tempo e iniciar contagem regressiva ao clicar no botão

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,13 +1,23 @@
 import style from "./Relogio.module.scss";
 
-export default function Relogio() {
+interface Props {
+  tempo: number | undefined;
+}
+
+export default function Relogio({ tempo = 0 }: Props) {
+  const minutos = Math.floor(tempo / 60);
+  const segundos = tempo % 60;
+
+  const [minDec, minUni] = String(minutos).padStart(2, "0").split("");
+  const [secDec, secUni] = String(segundos).padStart(2, "0").split("");
+
   return (
     <>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>{minDec}</span>
+      <span className={style.relogioNumero}>{minUni}</span>
       <span className={style.relogioDivisao}>:</span>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>{secDec}</span>
+      <span className={style.relogioNumero}>{secUni}</span>
     </>
   );
 }
