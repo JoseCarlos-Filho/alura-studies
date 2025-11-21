e66ab58e19c9deb83bb4e0fe116a216718d27607
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-11-20 22:48:07 -0300

    refatora componente Item para corrigir importação de estilos e melhorar a legibilidade do código

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,5 +1,5 @@
 import { ITarefa } from "../../../types/tarefa";
-import style from "../Lista.module.scss";
+import style from "./Item.module.scss";
 
 interface Props extends ITarefa {
   selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
@@ -13,10 +13,13 @@ export default function Item({
   id,
   selecionaTarefa,
 }: Props) {
-  console.log("item atual:", { tarefa, tempo, selecionado, completado, id });
+  // console.log("item atual:", { tarefa, tempo, selecionado, completado, id });
   return (
     <li
-      className={style.item}
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ""} ${
+        completado ? style.itemCompletado : ""
+      }`}
+      data-testid="item-tarefa"
       onClick={() =>
         !completado &&
         selecionaTarefa({
@@ -30,6 +33,9 @@ export default function Item({
     >
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
+      {completado && (
+        <span className={style.concluido} aria-label="tarefa completada"></span>
+      )}
     </li>
   );
 }
